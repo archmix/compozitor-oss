9d6a0f84a066acdfa5473589ab75f76657f7d9af
Braz <braz@mrbraz.tech>
2019-08-30 07:31:58 -0300

    Compozitor test

---
+++ compozitor-test/src/main/java/compozitor/processor/core/interfaces/FileObjectStringfy.java
@@ -0,0 +1,37 @@
+package compozitor.processor.core.interfaces;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+import com.google.testing.compile.Compilation;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(staticName = "create")
+public class FileObjectStringfy {
+  private final Compilation compilation;
+
+  public String sourceToString(String path) {
+    String resourceNotFound = String.format("Resource not found %s", path);
+    FileObject generatedFile = compilation.generatedFile(StandardLocation.SOURCE_OUTPUT, path)
+        .orElseThrow(() -> new RuntimeException(resourceNotFound));
+    return this.toString(generatedFile);
+  }
+  
+  public String resourceToString(String path) {
+    String resourceNotFound = String.format("Resource not found %s", path);
+    FileObject generatedFile = compilation.generatedFile(StandardLocation.CLASS_OUTPUT, path)
+        .orElseThrow(() -> new RuntimeException(resourceNotFound));
+    return this.toString(generatedFile);
+  }
+
+  public String toString(FileObject javaFile) {
+    try(InputStream input = javaFile.openInputStream()){
+      byte[] available = new byte[input.available()];
+      input.read(available);
+      return new String(available);
+    } catch(IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
---
+++ compozitor-test/src/test/resources/compozitor/processor/core/test/TypeModel.java
@@ -0,0 +1,22 @@
+package compozitor.processor.core.test;
+
+import java.util.List;
+
+import compozitor.processor.core.test.TypeAnnotation;
+
+@TypeAnnotation
+public class TypeModel {
+	private Long id;
+	
+	@FieldAnnotation
+	private long code;
+	
+	private String name;
+	
+	private List<String> values;
+	
+	@MethodAnnotation
+	public void doSomething(List<String> values) {
+		
+	}
+}
