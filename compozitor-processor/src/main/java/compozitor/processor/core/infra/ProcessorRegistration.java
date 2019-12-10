9bd10176a82e3664066c2965e6383c03ba41b467
Braz <braz@mrbraz.tech>
2019-12-10 07:35:26 -0300

    Auto Service is provided Auto Service alternative for registering annotation processor

---
+++ compozitor-processor/src/main/java/compozitor/processor/core/infra/ProcessorRegistration.java
@@ -0,0 +1,22 @@
+package compozitor.processor.core.infra;
+
+import com.google.auto.service.AutoService;
+import com.google.common.collect.Sets;
+import compozitor.processor.core.interfaces.Processor;
+import compozitor.processor.core.interfaces.ServiceProcessor;
+
+import java.util.Arrays;
+import java.util.Set;
+
+@AutoService(Processor.class)
+public class ProcessorRegistration extends ServiceProcessor {
+  @Override
+  public final Set<String> getSupportedAnnotationTypes() {
+    return Sets.newHashSet(Processor.class.getName());
+  }
+
+  @Override
+  protected final Iterable<Class<?>> serviceClasses() {
+    return Arrays.asList(javax.annotation.processing.Processor.class);
+  }
+}
