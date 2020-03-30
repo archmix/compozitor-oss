6573f25628982b7e53bd3dedfa585a0b6eb57599
Braz <mrbraz@archmix.org>
2020-03-30 15:05:44 -0300

    Boilerplate reduction

--- compozitor-processor/src/main/java/compozitor/processor/core/interfaces/Annotations.java
+++ compozitor-processor/src/main/java/compozitor/processor/core/interfaces/Annotations.java
@@ -6,11 +6,36 @@ import lombok.RequiredArgsConstructor;
 import javax.lang.model.element.AnnotationMirror;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 public class Annotations extends ModelIterable<AnnotationModel> {
+  private final JavaModel  javaModel;
 
   Annotations(List<? extends AnnotationMirror> annotations, JavaModel javaModel) {
     super(new AnnotationsSupplier(annotations, javaModel));
+    this.javaModel = javaModel;
+  }
+
+  public Optional<AnnotationModel> getBy(TypeModel typeModel){
+    return this.get(predicateOf(typeModel.getQualifiedName())) ;
+  }
+
+  public Optional<AnnotationModel> getBy(Class<?> typeClass) {
+    return this.get(predicateOf(typeClass.getName()));
+  }
+
+  public Optional<AnnotationModel> getBy(TypeName typeName) {
+    return this.get(predicateOf(typeName.toString()));
+  }
+
+  public Optional<AnnotationModel> getBy(String typeName) {
+    return this.get(predicateOf(typeName.toString()));
+  }
+
+  private Predicate<AnnotationModel> predicateOf(String type) {
+    TypeModel typeModel = javaModel.getType(type);
+    return (annotation) -> annotation.equals(typeModel);
   }
 
   @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
