346e425f2648608e62bddbac2a6546a6530d6734
Braz <mrbraz@archmix.org>
2020-04-14 11:07:50 -0300

    Always use UTF-8 as standard charset on templates

--- compozitor-template/src/main/java/compozitor/template/core/interfaces/TemplateEngine.java
+++ compozitor-template/src/main/java/compozitor/template/core/interfaces/TemplateEngine.java
@@ -3,6 +3,7 @@ package compozitor.template.core.interfaces;
 import org.apache.velocity.runtime.RuntimeServices;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class TemplateEngine {
   private final RuntimeServices engine;
@@ -16,7 +17,7 @@ public class TemplateEngine {
   }
 
   public Template getTemplate(String name) {
-    return new Template(this.engine.getTemplate(name));
+    return this.getTemplate(name, StandardCharsets.UTF_8);
   }
 
   public Template getTemplate(String name, Charset charset) {
